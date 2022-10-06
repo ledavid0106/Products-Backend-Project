a2761c1d5db09fed1d28de01095ed5c97797bfe5
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-06 09:53:07 -0700

    Created the GET, PUT, DELETE functionality of product_details and corrected the pathway for such

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework. response import Response
@@ -18,3 +19,20 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
