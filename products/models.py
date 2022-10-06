b5bcc452962890be7d02e46ea9cd41399a8ad3bc
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-06 11:16:18 -0700

    Fixed migration issue, changed url_image from charfield to textfield to consider possible long urls

--- products/models.py
+++ products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
-    url_image = models.CharField(max_length=10000000)
+    url_image = models.TextField(max_length=6000, default="", editable=False)
