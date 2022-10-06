5b04bbbb54ab86d69e79a7614332d36e3d5ceebe
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-06 09:04:15 -0700

    Updated Project URLS + Created View function + Created Products app url file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework. response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers
 
-# Create your views here.
+@api_view(['GET'])
+def products_list(request):
+
+    return Response('ok')
