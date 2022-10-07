58e9e238e42d7dee323b33e1b76fd911e0011ceb
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-07 08:39:25 -0700

    Created the products_review folder and funtionality + setting up the urls

---
+++ product_reviews/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from products.models import Product
+
+# Create your models here.
+
+class Review(models.Model):
+    review_text = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    name = models.CharField(max_length=255)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+  
